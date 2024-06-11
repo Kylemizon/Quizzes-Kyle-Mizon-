2e9e4eaa96750a8c2812d61f1b8ef235c970d47d
Kylemizon <k.mizon15@gmail.com>
2024-06-11 17:10:30 +0800

    Add files via upload

---
+++ quiz10.js
@@ -0,0 +1,15 @@
+function a() 
+{
+  document.getElementById("label1").style.color="red";
+  document.getElementById("label2").style.color = "green";
+}
+function b()
+{
+  document.getElementById("label2").style.color = "green";
+  
+}
+function c()
+{
+  document.getElementById("label2").style.color = "green";
+  document.getElementById("label3").style.color = "red";
+}
---
+++ quiz5.js
@@ -0,0 +1,14 @@
+function a() 
+{
+  document.getElementById("label1").style.color="green";
+}
+function b()
+{
+  document.getElementById("label1").style.color = "green";
+  document.getElementById("label2").style.color = "red";
+}
+function c()
+{
+  document.getElementById("label1").style.color = "green";
+  document.getElementById("label3").style.color = "red";
+}
